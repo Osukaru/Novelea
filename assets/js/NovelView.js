41153798ca29a5af0a919a80b7e0a4c7f7add0f4
Máximo Cuadros <mcuadros@mcuadros-laptop.lan>
2012-04-28 15:23:50 +0200

    mono

--- assets/js/NovelView.js
+++ assets/js/NovelView.js
@@ -6,22 +6,22 @@ var NovelView = Backbone.View.extend({
         'click #publish' : 'sendNovelFragment',
     },
 
-    initialize: function() {
-        this.socket = io.connect('http://192.168.3.75:8081');
-        
-        this.socket.on('getRedisDataResponse', function (data) {
-            console.log('data'+data);
-            for (var i = 0; i < data.length; i++) {
-                this.publishNovelFragment(data[i]);
-            };
-
-        });
-
-        this.socket.on('publish', this.publishNovelFragment);
-        
-        this.socket.on('connect', function (data) {
-            this.socket.emit('getRedisData', {});
-        });        
+    initialize: function() {  
+		console.log(this);      
+        var socket = io.connect('http://192.168.3.75:8081');
+		socket.on('getRedisDataResponse', function (data) {
+			for(var i=0;i<data.length;i++) {
+				console.log(data);
+				        var novel_fragment_template = _.template( $("#novel-fragment-template").html(), {novel_fragment_id: data[i].id, novel_fragment_text: data[i].text} );
+        $('#novel-fragment-list').append(novel_fragment_template); 
+			}
+		});
+		socket.on('publish', this.publishNovelFragment);
+		
+		socket.on('connect', function (data) {
+			socket.emit('getRedisData', {});
+		});
+		this.socket = socket;
     },
 
     sendNovelFragment: function(event) {
