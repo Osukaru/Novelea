ad261d5de370fd952255fc9a5be79056f2e058ea
Máximo Cuadros <mcuadros@mcuadros-laptop.lan>
2012-04-28 13:12:45 +0200

    Initial

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-var io = require('socket.io').listen(8080);
 
+var redis = require('redis');
+var client = redis.createClient();
+client.on('error', function(err) {
+	 console.log("Redis-Error " + err);		
+});
+
+var io = require('socket.io').listen(8081);
 io.sockets.on('connection', function (socket) {
-
-    socket.on('envio', function (data) {
-        
-        socket.broadcast.emit('respuesta', { respuesta: data.respuesta });
-        
-    });
-
+	socket.emit('news', { hello: 'world' });
+	socket.on('getRedisData', function (config) {
+		console.log('getRedisData', config);
+		if ( !config.pid ) config.pid = 0;
+		client.keys('{nf}{*}{'+config.pid+'}', function (err, replies) {
+			client.mget(replies, function (err, res) {
+				if ( !res ) return false;
+				
+				for(i=0;i<res.length;i++) { res[i] = eval('(' + res[i] + ')'); };
+				socket.emit('debug', res);
+			});
+			
+			console.log('DEBUG:::::' + replies.length + " replies:");
+			socket.emit('debug', replies);
+		});
+	});
+  
+	socket.on('setRedisData', function(data) {
+		console.log('setRedisData', data);
+		if ( !data.pid ) data.pid = 0;
+		data.id = new Date().getTime() + Math.random();
+		client.set('{nf}{'+data.id+'}{'+data.pid+'}', JSON.stringify(data), redis.print);
+		
+		socket.emit('publish', data);
+		socket.broadcast.emit('publish', data);
+	});
 });
