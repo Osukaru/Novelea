7602672e987645d36d2125dde4eed9e458700ccc
Osukaru <zepolracso@gmail.com>
2012-04-28 13:43:56 +0200

    Sockets

--- assets/js/NovelView.js
+++ assets/js/NovelView.js
@@ -3,16 +3,35 @@ var NovelView = Backbone.View.extend({
     el: '#novel',
 
     events: {
-        'click #publish' : 'publishNovelFragment',
+        'click #publish' : 'sendNovelFragment',
     },
 
     initialize: function() {
+        this.socket = io.connect('http://192.168.3.75:8081');
         
+        this.socket.on('getRedisDataResponse', function (data) {
+            console.log('data'+data);
+            for (var i = 0; i < data.length; i++) {
+                this.publishNovelFragment(data[i]);
+            };
+
+        });
+
+        this.socket.on('publish', this.publishNovelFragment);
+        
+        this.socket.on('connect', function (data) {
+            this.socket.emit('getRedisData', {});
+        });        
     },
 
-    publishNovelFragment: function(event) {
+    sendNovelFragment: function(event) {
         var novel_fragment_text = $(this.el).find('#novel-fragment-input').val();
-        var novel_fragment_template = _.template( $("#novel-fragment-template").html(), {novel_fragment_id: 1, novel_fragment_text: novel_fragment_text} );
+        this.socket.emit('setRedisData', { text: novel_fragment_text});
+        return false;
+    },
+
+    publishNovelFragment: function(data) {
+        var novel_fragment_template = _.template( $("#novel-fragment-template").html(), {novel_fragment_id: data.id, novel_fragment_text: data.text} );
         $('#novel-fragment-list').append(novel_fragment_template); 
         return false;
     },
